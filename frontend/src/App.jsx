312c7aadd6b7ec3ee32bfee89bdf8ee5a0ab5135
Unmesh <unmeshpatra2004@gmail.com>
2025-01-27 21:08:01 +0530

    home page done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,6 +10,7 @@ import Login from './pages/Login'
 import PlacedOrder from './pages/PlacedOrder'
 import Order from './pages/Order'
 import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
@@ -26,6 +27,7 @@ const App = () => {
         <Route path='/placedorder' element={<PlacedOrder />} />
         <Route path='/order' element={<Order />} />
       </Routes>
+      <Footer/>
     </div>
   )
 }
