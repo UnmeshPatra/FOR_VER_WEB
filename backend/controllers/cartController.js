975be6934258534eee03c4d94a0cae537a996884
Unmesh <unmeshpatra2004@gmail.com>
2025-02-19 21:19:28 +0530

    almost done

---
+++ backend/controllers/cartController.js
@@ -0,0 +1,20 @@
+
+
+//add product to user cart 
+const addToCart = async (req,res)=>{
+    
+}
+
+
+const updateCart = async (req,res)=>{
+
+}
+
+
+const getUserCart = async (req,res)=>{
+
+}
+
+
+
+export {addToCart,updateCart,getUserCart}
