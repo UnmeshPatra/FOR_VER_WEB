3d7d19e380321156ec410f52892a421d17c20042
Unmesh <unmeshpatra2004@gmail.com>
2025-04-01 00:04:04 +0530

    some

--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -42,6 +42,17 @@ const ShopContextProvider = (props) =>{
             cartData[itemId][size] = 1 ;
         }
         setCartItems(cartData);
+
+
+        if(token){
+            try {
+                await axios.post(backendUrl + '/api/cart/add',{itemId,size},{headers:{token}})
+            } catch (error) {
+                console.log(error);
+                toast.error(error.messsage)
+                
+            }
+        }
     }
 
 
@@ -69,7 +80,18 @@ const ShopContextProvider = (props) =>{
 
         setCartItems(cartData);
 
+        if(token){
+            try {
+                await axios.post(backendUrl + '/api/cart/update',{itemId,size,quantity},{headers:{token}})
+            } catch (error) {
+                console.log(error);
+                toast.error(error.messsage)
+                
+            }
+        }
+
     }
+
 
     const getCartAmount = () =>{
         let totalAmount = 0;
@@ -106,6 +128,18 @@ const ShopContextProvider = (props) =>{
         }
     }
 
+    const getUserCart = async(token)=>{
+        try {
+            const response = await axios.post(backendUrl + '/api/cart/get',{},{headers:{token}})
+            if(response.data.success){
+                setCartItems(response.data.cartData)
+
+            }
+        } catch (error) {
+            console.log(error);
+            toast.error(error.messsage)            
+        }
+    }
 
     useEffect(()=>{
         getProductData()
@@ -114,6 +148,7 @@ const ShopContextProvider = (props) =>{
     useEffect(()=>{
         if (!token && localStorage.getItem('token')) {
             setToken(localStorage.getItem('token'))
+            getUserCart(localStorage.getItem('token'))
         }
     },[])
 
