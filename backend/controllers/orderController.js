bf4416007e7c2819c5dba948d4da945f169254a3
Unmesh <unmeshpatra2004@gmail.com>
2025-04-03 19:37:04 +0530

    Fixed frontend folder issue

---
+++ backend/controllers/orderController.js
@@ -0,0 +1,35 @@
+
+
+// placing order using cod method 
+
+const placeOrder = async (req,res) =>{
+
+    
+}
+
+const placeOrderStripe = async (req,res) =>{
+
+
+}
+
+const placeOrderRazorPay = async (req,res) =>{
+
+
+}
+
+//all order for admin panel 
+const allOrders = async (req,res)=>{
+    
+}
+
+//user order data for frontend 
+const userOrders = async (req,res)=>{
+
+}
+
+//update order status from admin panel 
+const updateStatus = async (req,res)=>{
+
+}
+
+export {placeOrder,placeOrderRazorPay,placeOrderStripe,allOrders,userOrders,updateStatus}
